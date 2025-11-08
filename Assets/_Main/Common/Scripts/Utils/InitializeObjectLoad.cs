414e7aa2bd111483e34eed6e3346c1e7eec007c1
Truong0111 <truongquangtruong123@gmail.com>
2025-11-08 14:44:01 +0700

    chuyen folder sample -> common

--- Assets/_Main/Common/Scripts/Utils/InitializeObjectLoad.cs
+++ Assets/_Main/Common/Scripts/Utils/InitializeObjectLoad.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 public class InitializeObjectLoad : MonoBehaviour
 {
     [SerializeField] private GameObject[] objs;
-    [SerializeField] private bool dontDestroyOnLoad = true;
+    [SerializeField] private GameObject[] preloadObjs;
 
     private void Awake()
     {
         foreach (var obj in objs)
         {
             Instantiate(obj);
-            if (dontDestroyOnLoad) DontDestroyOnLoad(obj);
+        }
+
+        foreach (var preloadObj in preloadObjs)
+        {
+            Instantiate(preloadObj);
+            preloadObj.SetActive(false);
         }
     }
 }
