0b68063beb415459fd49b190f815687a8d185b7c
Truong0111 <truongquangtruong123@gmail.com>
2025-11-13 00:05:35 +0700

    update bai 1

--- Assets/_Main/Phan1/Bai1/Scripts/Object/ObjectPushBySlider.cs
+++ Assets/_Main/Phan1/Bai1/Scripts/Object/ObjectPushBySlider.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using _Main.Phan1.Bai1.Scripts.UI.WarningUI;
+using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
 
@@ -12,6 +13,8 @@ namespace _Main.Phan1.Bai1.Scripts
         public Slider slider;
         public float maxSpeed = 1f;
 
+        public string warningText;
+        
         public float reachPercent = .98f;
         public UnityEvent onReachValue;
         
@@ -45,7 +48,8 @@ namespace _Main.Phan1.Bai1.Scripts
 
             if (currentSpeed > maxSpeed)
             {
-                Debug.LogWarning($"Piston đẩy quá nhanh: {currentSpeed:F3} m/s (giới hạn {maxSpeed})");
+                WarningUI.Instance?.Show(warningText);
+                Debug.LogWarning($"Đẩy quá nhanh: {currentSpeed:F3} m/s (giới hạn {maxSpeed})");
             }
 
             piston.localPosition = startLocalPos + localDirection.normalized * (value * distance);
